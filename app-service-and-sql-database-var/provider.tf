5b876edfd7bb81d0e2804a3e7cefab3b51defb9e
Ajay Singh Chouhan <ajay2881@users.noreply.github.com>
2020-10-16 11:45:10 +0530

    Update provider.tf

--- app-service-and-sql-database-var/provider.tf
+++ app-service-and-sql-database-var/provider.tf
@@ -1,5 +1,8 @@
 provider "azurerm" {
-  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=2.4.0"
+  subscription_id = var.subscription_id
+  client_secret   = var.client_secret
+  tenant_id      = var.tenant_id
+  client_id= var.client_id
   features {}
 }
